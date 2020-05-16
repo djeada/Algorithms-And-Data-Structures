5a186396c24dd195e06922e984545001ec8a31a4
djeada <addjellouli1@gmail.com>
2020-05-16 16:36:05 +0200

    first

--- LeetCode/30-Day-Challange/src/Single Number.py
+++
@@ -1,9 +0,0 @@
-'''
-Given a non-empty array of integers, every element appears twice except for one. Find that single one.
-'''
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        x = 0
-        for i in nums:
-            x ^= i
-        return x
