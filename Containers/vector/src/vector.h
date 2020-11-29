8016fcfec46bfc9dc542d010b344d765a94cb68f
djeada <addjellouli1@gmail.com>
2020-11-29 20:08:10 +0100

    implemented example with vector

--- Containers/vector/src/vector.h
+++ Containers/vector/src/vector.h
@@ -2,6 +2,30 @@
 template<class T>
 class Vector {
 
+public:
+	Vector();
+	Vector(const Vector<T>& v);      
+	~Vector();
 
+	typedef T* iterator;
+	int size();
+	bool empty();
+	iterator begin();
+	iterator end();
+	T& front();
+	T& back();
+	void push_back(const T& value); 
+	T pop_back();  
+
+	void resize(unsigned int size);
+	T& operator[](unsigned int index);  
+	Vector<T> & operator=(const Vector<T> &);
+	void clear();
+
+private:
+	int n;
+	int capacity;
+	void reserve(unsigned int size);
+	T* data;
 };
 
--- Containers/vector/test/test_vector.cpp
+++ Containers/vector/test/test_vector.cpp
@@ -2,7 +2,43 @@
 #include "vector.h"
 
 TEST(VectorTest, AddingSingleElement) {
-	
+	Vector<int> v;
+
+	int value = 10;
+	int expectedSize = 1;
+
+	v.push_back(value);
+	EXPECT_EQ(v.size(), expectedSize);
+	EXPECT_EQ(v.back(), value);
 }
 
+TEST(VectorTest, AddingMultipleElements) {
+	Vector<char> v;
+
+	char value1 = 'a';
+	char value2 = 'b';
+	char value3 = 'c';
+	int expectedSize = 3;
+
+	v.push_back(value1);
+	v.push_back(value2);
+	v.push_back(value3);
+	EXPECT_EQ(v.pop_back(), value3);
+	EXPECT_EQ(v.pop_back(), value2);
+	EXPECT_EQ(v.pop_back(), value1);
+}
+
+TEST(VectorTest, CopyConstructor) {
+	Vector<int> original;
+
+	int value1 = 1;
+	int value2 = 2;
+	int value3 = 3;
+
+	Vector<int> copied(original);
+	
+	//EXPECT_EQ(original.pop_back(), copied.pop_back());
+	//EXPECT_EQ(original.pop_back(), copied.pop_back());
+	//EXPECT_EQ(original.pop_back(), copied.pop_back());
+}
 
