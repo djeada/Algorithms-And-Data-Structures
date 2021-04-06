47af55e4577670ce98ebee008dd066d660719d2e
djeada <addjellouli1@gmail.com>
2021-04-06 22:41:52 +0200

    formatting queue

--- src/containers/cpp/queue/src/queue.cpp
+++ src/containers/cpp/queue/src/queue.cpp
@@ -1,102 +1,113 @@
 #include "queue.h"
 #include <stdexcept>
 
-template<class T>
-Queue<T>::Queue() : 
-	data(new T[defaultInitSize]), 
-	n(0), 
-	maxSize(defaultInitSize),
- 	head(0), 
-	tail(0) {
-}
-       
-template<class T>
-Queue<T>::Queue(const Queue& other) {
-	copy(other);
-}
- 
-template<class T>
-Queue<T>::~Queue() {
-	delete[] data;
+template <class T>
+Queue<T>::Queue()
+    : data(new T[defaultInitSize])
+    , n(0)
+    , maxSize(defaultInitSize)
+    , head(0)
+    , tail(0)
+{
 }
 
-template<class T>
-void Queue<T>::copy(const Queue& other) {
-
-	n = other.n;
-	maxSize = other.maxSize;
-  	head = other.head;
-  	tail = other.tail;
-
-	data = new T[maxSize];
-
-	for (int i = 0; i < n; i++)
-		data[i] = other.data[i];
+template <class T>
+Queue<T>::Queue(const Queue& other)
+{
+    copy(other);
 }
 
-template<class T>
-void Queue<T>::expand() {
- 
-	int newMaxSize = maxSize * 2;
-	T* newData = new T[newMaxSize];
-
-	for (int i = 0; i < maxSize; i++)
-		newData[i] = data[i];
-
-	maxSize = newMaxSize;
-	head = 0;
- 	tail = maxSize;
-
-	T* oldData = data;
-	data = newData;
-	delete[] oldData;
+template <class T>
+Queue<T>::~Queue()
+{
+    delete[] data;
 }
 
-template<class T>
-T Queue<T>::dequeue() {
-	if (n <= 0) 
-		throw std::invalid_argument(invalidSizeMsg);
+template <class T>
+void Queue<T>::copy(const Queue& other)
+{
 
-	T element = data[head];
-	head = (head + 1) % maxSize;
-	n--;
+    n = other.n;
+    maxSize = other.maxSize;
+    head = other.head;
+    tail = other.tail;
 
-	return element;
+    data = new T[maxSize];
+
+    for (int i = 0; i < n; i++)
+        data[i] = other.data[i];
 }
 
-template<class T>
-void Queue<T>::enqueue(T element) {
-	if (n >= maxSize)
-		expand();
+template <class T>
+void Queue<T>::expand()
+{
 
-	data[tail] = element;
-	tail = (tail + 1) % maxSize;
-	n++;
+    int newMaxSize = maxSize * 2;
+    T* newData = new T[newMaxSize];
+
+    for (int i = 0; i < maxSize; i++)
+        newData[i] = data[i];
+
+    maxSize = newMaxSize;
+    head = 0;
+    tail = maxSize;
+
+    T* oldData = data;
+    data = newData;
+    delete[] oldData;
 }
 
-template<class T>
-T Queue<T>::peek() {
-  if (n <= 0) 
-	throw std::invalid_argument(invalidSizeMsg);
+template <class T>
+T Queue<T>::dequeue()
+{
+    if (n <= 0)
+        throw std::invalid_argument(invalidSizeMsg);
 
-  return data[n - 1];
+    T element = data[head];
+    head = (head + 1) % maxSize;
+    n--;
+
+    return element;
 }
 
-template<class T>
-int Queue<T>::size() {
-  return n;
+template <class T>
+void Queue<T>::enqueue(T element)
+{
+    if (n >= maxSize)
+        expand();
+
+    data[tail] = element;
+    tail = (tail + 1) % maxSize;
+    n++;
 }
 
-template<class T>
-bool Queue<T>::isEmpty() {
-  return n == 0;
+template <class T>
+T Queue<T>::peek()
+{
+    if (n <= 0)
+        throw std::invalid_argument(invalidSizeMsg);
+
+    return data[n - 1];
 }
 
-template<class T>
-Queue<T>& Queue<T>::operator=(const Queue<T>& other) {
-  delete[] data;
-  copy(other);
-  return *this;
+template <class T>
+int Queue<T>::size()
+{
+    return n;
+}
+
+template <class T>
+bool Queue<T>::isEmpty()
+{
+    return n == 0;
+}
+
+template <class T>
+Queue<T>& Queue<T>::operator=(const Queue<T>& other)
+{
+    delete[] data;
+    copy(other);
+    return *this;
 }
 
 template class Queue<int>;
--- src/containers/cpp/queue/src/queue.h
+++ src/containers/cpp/queue/src/queue.h
@@ -3,28 +3,26 @@
 const std::string invalidSizeMsg = "Invalid Size";
 const int defaultInitSize = 10;
 
-template<class T>
+template <class T>
 class Queue {
 
 public:
-	Queue();
-	Queue(const Queue& other);
-	~Queue();
-	void enqueue(T element);
-	T dequeue();
-	T peek();
-	int size();
-	bool isEmpty();
-	Queue& operator=(const Queue& other);
+    Queue();
+    Queue(const Queue& other);
+    ~Queue();
+    void enqueue(T element);
+    T dequeue();
+    T peek();
+    int size();
+    bool isEmpty();
+    Queue& operator=(const Queue& other);
 
 private:
-	T *data;
-	int head;
-	int tail;
-	int n;
-	int maxSize;
-	void copy(const Queue& other);
-	void expand();
-       
+    T* data;
+    int head;
+    int tail;
+    int n;
+    int maxSize;
+    void copy(const Queue& other);
+    void expand();
 };
-
