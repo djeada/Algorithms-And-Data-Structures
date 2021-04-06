1dd21671afa6f8b0a8dfb6bde96c66fd0f8a50a3
djeada <addjellouli1@gmail.com>
2021-04-06 22:40:14 +0200

    formatting vector

--- src/containers/cpp/vector/src/vector.cpp
+++ src/containers/cpp/vector/src/vector.cpp
@@ -1,26 +1,26 @@
 #include "vector.h"
 
-template<class T>
-Vector<T>::Vector() :
-    capacity(0),
-    n(0),
-    data(nullptr)
+template <class T>
+Vector<T>::Vector()
+    : capacity(0)
+    , n(0)
+    , data(nullptr)
 {
-
 }
 
-template<class T>
-Vector<T>::Vector(const Vector<T> & v)
+template <class T>
+Vector<T>::Vector(const Vector<T>& v)
 {
     n = v.n;
     capacity = v.capacity;
-    data = new T[n];  
+    data = new T[n];
     for (int i = 0; i < n; i++)
-        data[i] = v.data[i];  
+        data[i] = v.data[i];
 }
 
-template<class T>
-Vector<T> & Vector<T>::operator = (const Vector<T>& v) {
+template <class T>
+Vector<T>& Vector<T>::operator=(const Vector<T>& v)
+{
     delete[] data;
     n = v.n;
     capacity = v.capacity;
@@ -28,84 +28,96 @@ Vector<T> & Vector<T>::operator = (const Vector<T>& v) {
 
     for (int i = 0; i < n; i++)
         data[i] = v.data[i];
-    
-	return *this;
+
+    return *this;
 }
 
-template<class T>
-Vector<T>::~Vector() {
+template <class T>
+Vector<T>::~Vector()
+{
     delete[] data;
 }
 
-template<class T>
-typename Vector<T>::iterator Vector<T>::begin() {
+template <class T>
+typename Vector<T>::iterator Vector<T>::begin()
+{
     return data;
 }
 
-template<class T>
-typename Vector<T>::iterator Vector<T>::end() {
+template <class T>
+typename Vector<T>::iterator Vector<T>::end()
+{
     return data + size();
 }
 
-template<class T>
-T& Vector<T>::front() {
+template <class T>
+T& Vector<T>::front()
+{
     return data[0];
 }
 
-template<class T>
-T& Vector<T>::back() {
+template <class T>
+T& Vector<T>::back()
+{
     return data[n - 1];
 }
 
-template<class T>
-void Vector<T>::push_back(const T& v) {
+template <class T>
+void Vector<T>::push_back(const T& v)
+{
     if (n >= capacity)
         reserve(capacity + 5);
     data[n++] = v;
 }
 
-template<class T>
-T Vector<T>::pop_back() {
+template <class T>
+T Vector<T>::pop_back()
+{
     return data[n--];
 }
 
-template<class T>
-void Vector<T>::reserve(unsigned int newCapacity) {
+template <class T>
+void Vector<T>::reserve(unsigned int newCapacity)
+{
 
-	T* newData = new T[newCapacity];
-	capacity = newCapacity;
+    T* newData = new T[newCapacity];
+    capacity = newCapacity;
 
-	if (newCapacity < n)
-		n = newCapacity;	
+    if (newCapacity < n)
+        n = newCapacity;
 
-	for (int i = 0; i < n; i++)
-		newData[i] = data[i];
+    for (int i = 0; i < n; i++)
+        newData[i] = data[i];
 
-	delete[] data;
-	data = newData;
+    delete[] data;
+    data = newData;
 }
 
-template<class T>
-int Vector<T>::size() {
+template <class T>
+int Vector<T>::size()
+{
     return n;
 }
 
-template<class T>
-void Vector<T>::resize(unsigned int newSize) {
+template <class T>
+void Vector<T>::resize(unsigned int newSize)
+{
     reserve(newSize);
     n = newSize;
 }
 
-template<class T>
-T& Vector<T>::operator[](unsigned int index) {
+template <class T>
+T& Vector<T>::operator[](unsigned int index)
+{
     return data[index];
-}  
+}
 
 template <class T>
-void Vector<T>::clear() {
+void Vector<T>::clear()
+{
     capacity = 0;
     n = 0;
-	delete[] data;
+    delete[] data;
     data = nullptr;
 }
 
