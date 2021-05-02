58ed66832ad8d78c07aba59563525c16c8debf52
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-05-02 02:19:35 +0200

    Delete insertion_sort.cpp

--- src/sorting/cpp/insertion sort/insertion_sort.cpp
+++
@@ -1,9 +0,0 @@
-template<class FwdIt, class Compare = std::less<>>
-void insertion_sort(FwdIt first, FwdIt last, Compare cmp = Compare{})
-{
-    for (auto it = first; it != last; ++it) {
-        auto const insertion = std::upper_bound(first, it, *it, cmp);
-        std::rotate(insertion, it, std::next(it)); 
-        assert(std::is_sorted(first, std::next(it), cmp));
-    }
-}
