46247c630a3cd1655556667d31e988eb0aa6e2e8
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-05-06 02:42:46 +0200

    Update stack.py

--- src/collections/python/stack/stack.py
+++ src/collections/python/stack/stack.py
@@ -1,32 +1,3 @@
-"""
-It is an abstract data type (interface)
-
-Basic operations: pop(), push() and peek()
-
-LIFO structure: last in first out
-
-In most high level languages, a stack can be easily implemented either with arrays or linked lists
-
-A number of programming languages are stack-oriented, meaning they define most basic operations (adding two numbers, printing a character) as taking their arguments from the stack, and placing any return values back on the stack
-
-Push operation: put the given item to the top of the stack Very simple operation, can be done in O(1)
-
-Pop operation: we take the last item we have inserted to the top of the stack (LIFO) Very simple operation, can be done in O(1)
-
-Peek operation: return the item from the top of the stack without removing it Very simple operation, can be done in O(1)
-
-Applications:
-
-In stack-oriented programming languages
-
-Graph algorithms: depth-first search can be implemented with stacks ( or with recursion )
-
-Finding Euler-cycles in a graph
-
-Finding strongly connected components in a graph
-"""
-
-
 class Stack:
     def __init__(self):
         self.stack = []
