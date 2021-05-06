d2617298b004a9b9f13825f64895a4c906e4ad9c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-05-06 02:41:38 +0200

    Update queue.py

--- src/collections/python/queue/queue.py
+++ src/collections/python/queue/queue.py
@@ -1,28 +1,3 @@
-"""
-It is an abstract data type (interface)
-
-Basic operations: enqueue() and dequeue()
-
-FIFO: first in first out
-
-It can be implemented with linked lists
-
-Applications: BFS
-
-Enqueue operation: we just simply add the new item to the end of the queue
-
-Dequeue operation: we just simply remove the item starting at the beginning of the queue // FIFO structure
-
-Applications:
-
-When a resource is shared with several consumers ( threads ): we store them in a queue For example: CPU scheduling
-
-When data is transferred asynchronously (data not necessarily received at same rate as sent) between two processes For example: IO buffers
-"""
-
-# FIFO - first -> first
-
-
 class Queue:
     def __init__(self):
         self.queue = []
