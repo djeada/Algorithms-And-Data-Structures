115f52821ae8481afdd92735492c752751c3ffdf
djeada <addjellouli1@gmail.com>
2021-05-26 23:48:43 +0200

    completed all construct example

--- src/dynamic programming/cpp/all construct/src/all_construct.h
+++ src/dynamic programming/cpp/all construct/src/all_construct.h
@@ -3,4 +3,12 @@
 
 std::vector<std::vector<std::string>>
 allConstructBasic(const std::string &target,
-                  const std::vector<std::string> &wordBank); 
+                  const std::vector<std::string> &wordBank);
+
+std::vector<std::vector<std::string>>
+allConstructMemo(const std::string &target,
+                 const std::vector<std::string> &wordBank);
+
+std::vector<std::vector<std::string>>
+allConstructTable(const std::string &target,
+                  const std::vector<std::string> &wordBank);
