06d4e09965ea439868a2e32ca814567f9e1b64ca
djeada <addjellouli1@gmail.com>
2021-06-04 01:40:50 +0200

    generalized Graph class with templates

--- src/graphs/cpp/graph/test/test_graph.cpp
+++ src/graphs/cpp/graph/test/test_graph.cpp
@@ -4,72 +4,104 @@
 #include <algorithm>
 
 template <typename T>
-void assertVectorsEqual(std::vector<T> x, std::vector<T> y)
-{
-    ASSERT_EQ(x.size(), y.size()) << "Vectors x and y are of unequal length";
-    
-    sort(x.begin(), x.end());
-    sort(y.begin(), y.end());
+void assertVectorsEqual(std::vector<T> x, std::vector<T> y) {
+  ASSERT_EQ(x.size(), y.size()) << "Vectors x and y are of unequal length";
 
-    for (int i = 0; i < x.size(); ++i) {
-        EXPECT_EQ(x[i], y[i]) << "Vectors x and y differ at index " << i;
-    }
-}
-TEST(Graph, DefaultConstructor)
-{
-    Graph graph;
-    ASSERT_TRUE(graph.empty());
+  sort(x.begin(), x.end());
+  sort(y.begin(), y.end());
+
+  for (int i = 0; i < x.size(); ++i) {
+    EXPECT_EQ(x[i], y[i]) << "Vectors x and y differ at index " << i;
+  }
 }
 
-TEST(Graph, AddingEdges)
-{
-    Graph graph;
-    graph.addEdge(Vertex(1), Vertex(5), 10);
-    graph.addEdge(Vertex(1), Vertex(2), 10);
-    graph.addEdge(Vertex(1), Vertex(3), 10);
-    graph.addEdge(Vertex(2), Vertex(2), 1);
-    graph.addEdge(Vertex(3), Vertex(3), 2);
-    graph.addEdge(Vertex(4), Vertex(-5), 3);
-    EXPECT_EQ(graph.size(), 6);
-    ASSERT_TRUE(graph.contains(1));
-    ASSERT_TRUE(graph.contains(2));
-    ASSERT_TRUE(graph.contains(3));
-    ASSERT_TRUE(graph.contains(4));
+TEST(Vertex, EqualInt) {
+  auto vertexA = Vertex<int>(1);
+  auto vertexB = Vertex<int>(1);
 
-    assertVectorsEqual(graph.edges(Vertex(1)),
-        std::vector<Edge>{
-            Edge(Vertex(1), Vertex(5), 10),
-            Edge(Vertex(1), Vertex(2), 10),
-            Edge(Vertex(1), Vertex(3), 10) });
-
-    assertVectorsEqual(graph.edges(Vertex(2)),
-        std::vector<Edge>{
-            Edge(Vertex(2), Vertex(2), 1) });
-
-    assertVectorsEqual(graph.edges(Vertex(3)),
-        std::vector<Edge>{
-            Edge(Vertex(3), Vertex(3), 2) });
-
-    assertVectorsEqual(graph.edges(Vertex(4)),
-        std::vector<Edge>{
-            Edge(Vertex(4), Vertex(-5), 3) });
+  EXPECT_EQ(vertexA, vertexB);
 }
 
-TEST(Graph, AddingVertices)
-{
-    Graph graph;
-    graph.addVertex(Vertex(1));
-    graph.addVertex(Vertex(2));
-    graph.addVertex(Vertex(3));
+TEST(Vertex, EqualChar) {
+  auto vertexA = Vertex<char>('A');
+  auto vertexB = Vertex<char>('A');
 
-    ASSERT_TRUE(graph.contains(1));
-    ASSERT_TRUE(graph.contains(2));
-    ASSERT_TRUE(graph.contains(3));
-    EXPECT_EQ(graph.size(), 3);
+  EXPECT_EQ(vertexA, vertexB);
+}
 
-    assertVectorsEqual(graph.vertices(),
-        std::vector<Vertex>{
-            Vertex(1),
-            Vertex(2),
-            Vertex(3) });
+TEST(Vertex, NotEqualInt) {
+  auto vertexA = Vertex<int>(1);
+  auto vertexB = Vertex<int>(2);
+
+  EXPECT_NE(vertexA, vertexB);
+}
+
+TEST(Vertex, NotEqualChar) {
+  auto vertexA = Vertex<char>('A');
+  auto vertexB = Vertex<char>('B');
+
+  EXPECT_NE(vertexA, vertexB);
+}
+
+TEST(Vertex, Sort) {
+  std::vector<Vertex<int>> array = {Vertex<int>(5), Vertex<int>(4), Vertex<int>(1), Vertex<int>(3), Vertex<int>(2)};
+  std::vector<Vertex<int>> expectedResult = {Vertex<int>(1), Vertex<int>(2), Vertex<int>(3), Vertex<int>(4), Vertex<int>(5)};
+  std::sort(array.begin(), array.end());
+
+  assertVectorsEqual(array, expectedResult);
+}
+
+TEST(Graph, DefaultConstructor) {
+  Graph<int> graph;
+  ASSERT_TRUE(graph.empty());
+}
+
+TEST(Graph, AddingVertices) {
+  Graph<int> graph;
+  graph.addVertex(Vertex<int>(1));
+  graph.addVertex(Vertex<int>(2));
+  graph.addVertex(Vertex<int>(3));
+
+  ASSERT_TRUE(graph.contains(1));
+  ASSERT_TRUE(graph.contains(2));
+  ASSERT_TRUE(graph.contains(3));
+  EXPECT_EQ(graph.size(), 3);
+
+  assertVectorsEqual(
+      graph.vertices(),
+      std::vector<Vertex<int>>{Vertex<int>(1), Vertex<int>(2), Vertex<int>(3)});
+}
+
+TEST(Graph, AddingEdges) {
+  Graph<int> graph;
+  graph.addEdge(Vertex<int>(1), Vertex<int>(5), 10);
+  graph.addEdge(Vertex<int>(1), Vertex<int>(2), 10);
+  graph.addEdge(Vertex<int>(1), Vertex<int>(3), 10);
+  graph.addEdge(Vertex<int>(2), Vertex<int>(2), 1);
+  graph.addEdge(Vertex<int>(3), Vertex<int>(3), 2);
+  graph.addEdge(Vertex<int>(4), Vertex<int>(-5), 3);
+  EXPECT_EQ(graph.size(), 6);
+
+  ASSERT_TRUE(graph.contains(1));
+  ASSERT_TRUE(graph.contains(2));
+  ASSERT_TRUE(graph.contains(3));
+  ASSERT_TRUE(graph.contains(4));
+
+  assertVectorsEqual(
+      graph.edges(Vertex<int>(1)),
+      std::vector<Edge<int>>{Edge<int>(Vertex<int>(1), Vertex<int>(5), 10),
+                             Edge<int>(Vertex<int>(1), Vertex<int>(2), 10),
+                             Edge<int>(Vertex<int>(1), Vertex<int>(3), 10)});
+
+  assertVectorsEqual(
+      graph.edges(Vertex<int>(2)),
+      std::vector<Edge<int>>{Edge<int>(Vertex<int>(2), Vertex<int>(2), 1)});
+
+  assertVectorsEqual(
+      graph.edges(Vertex<int>(3)),
+      std::vector<Edge<int>>{Edge<int>(Vertex<int>(3), Vertex<int>(3), 2)});
+
+  assertVectorsEqual(
+      graph.edges(Vertex<int>(4)),
+      std::vector<Edge<int>>{Edge<int>(Vertex<int>(4), Vertex<int>(-5), 3)});
 }
